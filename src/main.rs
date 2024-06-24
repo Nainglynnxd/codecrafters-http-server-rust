f3630397d8dacedf9c1f43cd62eb82285f03fdbb
Naing Lynn <nainglynnxd@gmail.com>
2024-06-24 20:26:45 +0100

    pass stage

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 // Uncomment this block to pass the first stage
 use std::io::{Read, Write};
-use std::net::TcpListener;
+use std::net::{TcpListener, TcpStream};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -9,21 +9,42 @@ fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
     for stream in listener.incoming() {
         match stream {
-            Ok(mut stream) => {
+            Ok(stream) => {
                 println!("accepted new connection");
-                let mut buffer = [0; 1024];
-                stream.read(&mut buffer).unwrap();
-
-                let response = if buffer.starts_with(b"GET / HTTP/1.1\r\n") {
-                    "HTTP/1.1 200 OK\r\n\r\n"
-                } else {
-                    "HTTP/1.1 404 Not Found\r\n\r\n"
-                };
-                stream.write(response.as_bytes()).unwrap();
+                handle_connection(stream);
             }
             Err(e) => {
                 println!("error: {}", e);
             }
+        }
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
+
+    let request = String::from_utf8_lossy(&buffer);
+    let mut lines = request.lines();
+    if let Some(request_line) = lines.next() {
+        let parts: Vec<&str> = request_line.split_whitespace().collect();
+        if parts.len() == 3 {
+            let request_header = parts[0] == "GET";
+            let path = parts[1];
+
+            let response = if request_header && path.starts_with("/echo/") {
+                let response_body = path.trim_start_matches("/echo/");
+                format!(
+                    "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
+                    response_body.len(),
+                    response_body
+                )
+            } else if request_header && path == "/" {
+                format!("HTTP/1.1 200 OK\r\n\r\n")
+            } else {
+                format!("HTTP/1.1 404 Not Found\r\n\r\n")
+            };
+            stream.write(response.as_bytes()).unwrap();
         }
     }
 }
