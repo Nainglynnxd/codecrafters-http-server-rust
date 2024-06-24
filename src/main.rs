3fd64700904ab76249b809c28f2ce095488df7a5
Naing Lynn <nainglynnxd@gmail.com>
2024-06-24 22:30:48 +0100

    pass stage

--- src/main.rs
+++ src/main.rs
@@ -25,22 +25,38 @@ fn handle_connection(mut stream: TcpStream) {
     stream.read(&mut buffer).unwrap();
 
     let request = String::from_utf8_lossy(&buffer);
-    let mut lines = request.lines();
-    if let Some(request_line) = lines.next() {
+
+    let mut headers = request.lines();
+    if let Some(request_line) = headers.next() {
         let parts: Vec<&str> = request_line.split_whitespace().collect();
         if parts.len() == 3 {
             let request_header = parts[0] == "GET";
             let path = parts[1];
 
-            let response = if request_header && path.starts_with("/echo/") {
+            let mut user_agent = "unknown";
+
+            for header in headers {
+                if header.starts_with("User-Agent") {
+                    user_agent = header.trim_start_matches("User-Agent:").trim();
+                    break;
+                }
+            }
+
+            let response = if request_header && path == "/user-agent" {
+                format!(
+                    "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
+                    user_agent.len(),
+                    user_agent
+                )
+            } else if request_header && path == "/" {
+                format!("HTTP/1.1 200 OK\r\n\r\n")
+            } else if request_header && path.starts_with("/echo/") {
                 let response_body = path.trim_start_matches("/echo/");
                 format!(
                     "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
                     response_body.len(),
                     response_body
                 )
-            } else if request_header && path == "/" {
-                format!("HTTP/1.1 200 OK\r\n\r\n")
             } else {
                 format!("HTTP/1.1 404 Not Found\r\n\r\n")
             };
