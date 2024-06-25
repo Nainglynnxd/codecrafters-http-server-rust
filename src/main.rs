0726dc3997362c4d661a3500611491f650a7dd05
Naing Lynn <nainglynnxd@gmail.com>
2024-06-25 14:32:15 +0100

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,22 @@
-// Uncomment this block to pass the first stage
+use std::fs;
 use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
 use std::thread;
 
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
-                println!("accepted new connection");
+                println!("Accepted new connection");
                 thread::spawn(|| {
                     handle_connection(stream);
                 });
             }
             Err(e) => {
-                println!("error: {}", e);
+                println!("Error: {}", e);
             }
         }
     }
@@ -25,49 +24,68 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
-    stream.read(&mut buffer).unwrap();
+    match stream.read(&mut buffer) {
+        Ok(_) => {
+            let request = String::from_utf8_lossy(&buffer);
 
-    let request = String::from_utf8_lossy(&buffer);
+            let mut req_lines = request.lines();
+            if let Some(request_line) = req_lines.next() {
+                let parts: Vec<&str> = request_line.split_whitespace().collect();
+                if parts.len() == 3 {
+                    let request_method = parts[0];
+                    let path = parts[1];
 
-    let mut req_body = request.lines();
-    if let Some(request_line) = req_body.next() {
-        let parts: Vec<&str> = request_line.split_whitespace().collect();
-        if parts.len() == 3 {
-            let request_header = parts[0] == "GET";
-            let path = parts[1];
+                    let mut user_agent = "";
 
-            let mut user_agent = "";
+                    for header in req_lines {
+                        if header.starts_with("User-Agent:") {
+                            user_agent = header.trim_start_matches("User-Agent:").trim();
+                            break;
+                        }
+                    }
 
-            for header in req_body {
-                if header.starts_with("User-Agent") {
-                    user_agent = header.trim_start_matches("User-Agent:").trim();
-                    break;
+                    let response = if request_method == "GET" {
+                        match path {
+                            "/" => "HTTP/1.1 200 OK\r\n\r\n".to_owned(),
+                            f if f.starts_with("/files/") => {
+                                let filename = &path[7..];
+                                println!("Path: {filename}");
+                                match fs::read(format!("/tmp/data/codecrafters.io/http-server-tester/{}", filename)) {
+                                    Ok(contents) => {
+                                        format!(
+                                            "HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {}\r\n\r\n{}",
+                                            contents.len(),
+                                            String::from_utf8_lossy(&contents)
+                                        )
+                                    }
+                                    Err(_) => "HTTP/1.1 404 Not Found\r\n\r\n".to_owned(),
+                                }
+                            }
+                            p if p.starts_with("/echo/") => {
+                                let response_body = path.trim_start_matches("/echo/");
+                                format!(
+                                    "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
+                                    response_body.len(),
+                                    response_body
+                                )
+                            }
+                            "/user-agent" => format!(
+                                "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
+                                user_agent.len(),
+                                user_agent
+                            ),
+                            _ => "HTTP/1.1 404 Not Found\r\n\r\n".to_owned(),
+                        }
+                    } else {
+                        "HTTP/1.1 405 Method Not Allowed\r\n\r\n".to_owned()
+                    };
+
+                    stream.write_all(response.as_bytes()).unwrap();
                 }
             }
-
-            let response = if request_header {
-                match path {
-                    "/" => "HTTP/1.1 200 OK\r\n\r\n".to_owned(),
-                    p if p.starts_with("/echo/") => {
-                        let response_body = path.trim_start_matches("/echo/");
-                        format!(
-                            "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
-                            response_body.len(),
-                            response_body
-                        )
-                    }
-                    "/user-agent" => format!(
-                        "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
-                        user_agent.len(),
-                        user_agent
-                    ),
-                    _ => "HTTP/1.1 404 Not Found\r\n\r\n".to_owned(),
-                }
-            } else {
-                "HTTP/1.1 405 Method Not Allowed\r\n\r\n".to_owned()
-            };
-
-            stream.write_all(response.as_bytes()).unwrap();
+        }
+        Err(e) => {
+            eprintln!("Failed to read from stream: {}", e);
         }
     }
 }
