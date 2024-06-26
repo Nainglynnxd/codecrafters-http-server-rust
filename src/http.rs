2959d6d5f7c0a87b17a332c366ae2dd5341355d3
Naing Lynn <nainglynnxd@gmail.com>
2024-06-26 13:31:44 +0100

    Refactor: echo request

--- src/http.rs
+++ src/http.rs
@@ -13,17 +13,31 @@ impl StatusCode {
         }
     }
 
-    pub fn _compose_response(&self, body: &str) -> String {
-        format!("HTTP/1.1 {}\r\n{}", self.as_str(), body)
+    pub fn part(&self) -> String {
+        format!("HTTP/1.1 {}\r\n", self.as_str())
     }
 
-    pub fn default(&self) -> String {
+    pub fn whole(&self) -> String {
         format!("HTTP/1.1 {}\r\n\r\n", self.as_str())
     }
 }
 
+#[derive(Debug)]
 pub enum Request {
     GET,
     POST,
     NONE,
+}
+
+pub fn extract_request_method_and_path(header_line: Option<&str>) -> (Request, &str) {
+    let request: Vec<&str> = header_line.unwrap().split_whitespace().collect();
+    let method = request[0];
+    let path = request[1];
+    use Request::*;
+    let val = match method {
+        "GET" => GET,
+        "POST" => POST,
+        _ => NONE,
+    };
+    (val, path)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
+#![allow(dead_code, unused_imports)]
 mod http;
+use http::extract_request_method_and_path;
 use http::Request::{GET, NONE, POST};
 use http::StatusCode::{Created, NotFound, OK};
 
@@ -28,109 +30,28 @@ fn handle_connection(mut stream: TcpStream) {
     match stream.read(&mut buffer) {
         Ok(size) => {
             let request = String::from_utf8_lossy(&buffer[..size]);
-            let mut req_lines = request.lines();
-            if let Some(request_line) = req_lines.next() {
-                let parts: Vec<&str> = request_line.split_whitespace().collect();
-                if parts.len() == 3 {
-                    let request_method = match parts[0] {
-                        "GET" => GET,
-                        "POST" => POST,
-                        _ => NONE,
-                    };
-
-                    let path = parts[1];
-
-                    let mut user_agent = "";
-                    let mut content_length = 0;
-
-                    for header in req_lines.clone() {
-                        if header.starts_with("User-Agent:") {
-                            user_agent = header.trim_start_matches("User-Agent:").trim();
-                        } else if header.starts_with("Content-Length:") {
-                            content_length = header
-                                .trim_start_matches("Content-Length:")
-                                .trim()
-                                .parse()
-                                .unwrap_or(0);
-                        }
-                    }
-
-                    let response = match request_method {
-                        GET => match path {
-                            "/" => OK.default(),
-                            f if f.starts_with("/files/") => {
-                                let filename = &path[7..];
-                                match fs::read(format!("/tmp/data/codecrafters.io/http-server-tester/{}", filename)) {
-                                    Ok(contents) => {
-                                        format!(
-                                            "{}Content-Type: application/octet-stream\r\nContent-Length: {}\r\n\r\n{}",
-                                            OK.default(),
-                                            contents.len(),
-                                            String::from_utf8_lossy(&contents)
-                                        )
-                                    }
-                                    Err(_) => NotFound.default()
-                                }
-                            }
-                            p if p.starts_with("/echo/") => {
-                                let response_body = path.trim_start_matches("/echo/");
-                                format!("{}Content-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
-                                    OK.default(),
-                                    response_body.len(),
-                                    response_body
-                                )
-                            }
-                            "/user-agent" => format!(
-                                "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
-                                user_agent.len(),
-                                user_agent
-                            ),
-
-                            _ => NotFound.default()
-                        },
-                        POST => match path {
-                            p if p.starts_with("/files/") => {
-                                let filename = &path[7..];
-
-                                let mut headers_done = false;
-                                let mut body_start_index = 0;
-
-                                for (i, line) in request.lines().enumerate() {
-                                    if line.is_empty() {
-                                        headers_done = true;
-                                        body_start_index = i + 1;
-                                        break;
-                                    }
-                                }
-
-                                if headers_done {
-                                    let body = &buffer[size - content_length..size];
-                                    let file_path = format!("/tmp/data/codecrafters.io/http-server-tester/{}", filename);
-                                    match fs::write(&file_path, body) {
-                                        Ok(_) => Created.default(),
-                                        Err(e) => {
-                                            eprintln!("Failed to write to file: {}", e);
-                                            "HTTP/1.1 500 Internal Server Error\r\n\r\n".to_owned()
-                                        }
-                                    }
-                                } else {
-                                    "HTTP/1.1 400 Bad Request\r\n\r\n".to_owned()
-                                }
-                            }
-
-                            _ => NotFound.default()
-                        },
-                        _ => "HTTP/1.1 405 Method Not Allowed\r\n\r\n".to_owned(),
-                    };
-
-                    if !response.is_empty() {
-                        stream.write_all(response.as_bytes()).unwrap();
-                    }
-                }
-            }
+            let response = match extract_request_method_and_path(request.lines().next()) {
+                (GET, path) => match path {
+                    "/" => OK.whole(),
+                    p if p.starts_with("/echo/") => echo(path),
+                    _ => NotFound.whole(),
+                },
+                (POST, "") => OK.whole(),
+                (NONE, "") => String::new(),
+                _ => String::new(),
+            };
+            stream.write(response.as_bytes()).unwrap();
         }
-        Err(e) => {
-            eprintln!("Failed to read from stream: {}", e);
-        }
+        Err(_) => {}
     }
+}
+
+fn echo(path: &str) -> String {
+    let response = path.trim_start_matches("/echo/");
+    format!(
+        "{}Content-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
+        OK.part(),
+        response.len(),
+        response
+    )
 }
