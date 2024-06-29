fb0f1888f9afb3109878a1a50e330c93c9df5f3d
Naing Lynn <nainglynnxd@gmail.com>
2024-06-29 10:19:31 +0100

    Refactor: Complete

--- src/http.rs
+++ src/http.rs
@@ -1,20 +1,71 @@
-#![allow(dead_code)]
-pub struct HttpRequest {
-    pub path: String,
+#[derive(Debug)]
+pub struct Request {
+    pub method: String,
+    pub route: String,
     pub user_agent: Option<String>,
-    pub valid_encoding: Option<String>,
+    pub encoding: Option<String>,
+    pub body: String,
 }
 
-impl Default for HttpRequest {
-    /// Default values for the Request
-    fn default() -> Self {
-        HttpRequest {
-            path: Default::default(),
-            user_agent: Default::default(),
-            valid_encoding: Default::default(),
+pub struct Response {
+    pub status_line: Vec<u8>,
+    pub headers: Vec<u8>,
+    pub body: Vec<u8>,
+}
+
+impl Response {
+    pub fn new() -> Self {
+        Response {
+            status_line: Vec::new(),
+            headers: Vec::new(),
+            body: Vec::new(),
         }
     }
-}
 
-//
-// handle_connection(stream, &request_string, directory) {}
+    pub fn ok(mut self) -> Self {
+        self.status_line = b"HTTP/1.1 200 OK\r\n".to_vec();
+        self
+    }
+
+    pub fn created(mut self) -> Self {
+        self.status_line = b"HTTP/1.1 201 Created\r\n".to_vec();
+        self
+    }
+
+    pub fn not_found(mut self) -> Self {
+        self.status_line = b"HTTP/1.1 404 Not Found\r\n".to_vec();
+        self
+    }
+
+    pub fn content_encoding(mut self) -> Self {
+        self.headers
+            .extend_from_slice(b"Content-Encoding: gzip\r\n");
+        self
+    }
+
+    pub fn content_type(mut self, content_type: &str) -> Self {
+        self.headers
+            .extend_from_slice(format!("Content-Type: {}\r\n", content_type).as_bytes());
+        self
+    }
+
+    pub fn content_length(mut self, len: usize) -> Self {
+        self.headers
+            .extend_from_slice(format!("Content-Length: {}\r\n", len).as_bytes());
+        self
+    }
+
+    pub fn body(mut self, body: Vec<u8>) -> Self {
+        self.body = body;
+        self
+    }
+
+    pub fn build(self) -> Vec<u8> {
+        let mut response = Vec::new();
+        response.extend_from_slice(&self.status_line);
+        response.extend_from_slice(&self.headers);
+        response.extend_from_slice(b"\r\n");
+        response.extend_from_slice(&self.body);
+        response
+    }
+}
--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,36 +1,129 @@
-#![allow(dead_code)]
-use super::*;
+use anyhow::{Error, Result};
+use flate2::{write::GzEncoder, Compression};
+use std::{fs, fs::File, io::Write, net::TcpStream};
 
-pub fn parse_request(request: &str) -> Result<HttpRequest, Error> {
-    let lines = request.lines().collect::<Vec<&str>>();
-    let mut req_header = lines[0].split_whitespace();
-    let user_agent = parse_header("User-Agent: ", &lines);
-    let encoding = parse_header("Accept-Encoding: ", &lines);
-    let http_request = HttpRequest {
-        path: String::from(req_header.nth(1).unwrap_or("")),
-        user_agent: Some(user_agent),
-        valid_encoding: Some(encoding),
+use super::{Request, Response};
+
+pub fn handle_connection(mut stream: TcpStream, request: &str, directory: String) {
+    let http_request = parse_request(request);
+    let Request {
+        method,
+        route,
+        user_agent,
+        encoding,
+        body,
+    } = http_request.unwrap();
+
+    let response = match method.as_str() {
+        "GET" => match route.as_str() {
+            "/" => Response::new().ok().content_length(0).build(),
+            route if route.starts_with("/echo/") => {
+                let content = route.replace("/echo/", "");
+
+                let res = Response::new().ok().content_type("text/plain");
+                if encoding.unwrap().contains("gzip") {
+                    let mut encoder = GzEncoder::new(Vec::new(), Compression::default());
+                    encoder.write_all(content.as_bytes()).unwrap();
+                    let compressed = encoder.finish().unwrap();
+                    res.content_encoding()
+                        .content_length(compressed.len())
+                        .body(compressed)
+                        .build()
+                } else {
+                    res.content_length(content.len())
+                        .body(content.into_bytes())
+                        .build()
+                }
+            }
+            route if route.starts_with("/user-agent") => {
+                let agent = user_agent.unwrap();
+
+                Response::new()
+                    .ok()
+                    .content_type("text/plain")
+                    .content_length(agent.len())
+                    .body(agent.into_bytes())
+                    .build()
+            }
+            route if route.starts_with("/files/") => {
+                let filename = route.replace("/files/", "");
+                let mut filepath = directory;
+                if !filepath.ends_with('/') {
+                    filepath.push('/');
+                }
+                filepath.push_str(&filename);
+                match fs::read(&filepath) {
+                    Ok(content) => Response::new()
+                        .ok()
+                        .content_type("application/octet-stream")
+                        .content_length(content.len())
+                        .body(content)
+                        .build(),
+                    Err(e) => {
+                        eprintln!("Failed to read the file: {e}");
+                        Response::new().not_found().content_length(0).build()
+                    }
+                }
+            }
+            _ => Response::new().not_found().content_length(0).build(),
+        },
+        "POST" => {
+            let filename = route.strip_prefix("/files/").unwrap();
+            let mut filepath = directory;
+            if !filepath.ends_with('/') {
+                filepath.push('/');
+            }
+            filepath.push_str(filename);
+            let mut file = match File::create(&filepath) {
+                Ok(f) => f,
+                Err(e) => {
+                    eprintln!("Failed to create file: {e}");
+                    return;
+                }
+            };
+
+            if let Err(e) = file.write_all(body.as_bytes()) {
+                eprintln!("Failed to write to file: {e}");
+            }
+
+            Response::new().created().content_length(0).build()
+        }
+        _ => b"Invalid request method".to_vec(),
     };
-    Ok(http_request)
-}
 
-pub fn parse_header(header: &str, lines: &[&str]) -> String {
-    lines
-        .iter()
-        .find(|line| line.starts_with(header))
-        .unwrap_or(&"")
-        .replace(header, "")
-}
-
-pub fn file_path_exists(path: &str) -> bool {
-    match fs::metadata(path) {
-        Ok(metadata) => {
-            println!("File metadata for path {}: {:?}", path, metadata);
-            metadata.is_file()
-        }
-        Err(e) => {
-            println!("Failed to get metadata for path {}: {}", path, e);
-            false
-        }
+    if let Err(e) = stream.write_all(&response) {
+        println!("Failed to send response: {}", e);
     }
+}
+
+pub fn parse_request(request: &str) -> Result<Request, Error> {
+    let (method_line, headers) = request.split_once("\r\n").unwrap();
+    let (_, body) = headers.split_once("\r\n\r\n").unwrap();
+
+    let method_line: Vec<&str> = method_line.split_whitespace().collect();
+    let result = parse_header(headers);
+
+    Ok(Request {
+        method: String::from(method_line[0]),
+        route: String::from(method_line[1]),
+        user_agent: result.0,
+        encoding: result.1,
+        body: String::from(body),
+    })
+}
+
+pub fn parse_header(headers: &str) -> (Option<String>, Option<String>) {
+    let lines = headers.lines().collect::<Vec<&str>>();
+    let user_agent = lines
+        .iter()
+        .find(|line| line.starts_with("User-Agent: "))
+        .unwrap_or(&"")
+        .replace("User-Agent: ", "");
+    let encoding = lines
+        .iter()
+        .find(|line| line.starts_with("Accept-Encoding: "))
+        .unwrap_or(&"")
+        .replace("Accept-Encoding: ", "");
+
+    (Some(user_agent), Some(encoding))
 }
